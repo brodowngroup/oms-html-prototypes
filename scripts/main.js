e72d084fbb59a02ee305eda955d4357007bb7b17
brodowngroup <mike@codegreendevelopment.com>
2012-11-17 16:04:03 -0600

    Adds links to seperate prototype pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// HEADER SEARCH TOGGLE
 var toggleSearch = function() {
   var $form = $('form.header_search'),
       height = -($form.height());
@@ -6,8 +7,48 @@ var toggleSearch = function() {
     $form.animate({'top': height}, 100).removeClass('active') :
     $form.animate({'top': 0}, 100).addClass('active');
 
+},
+adjustContainers = function() {
+    var headerHeight = $('header').height(),
+        searchHeight = $('form.header_search').height(),
+        containerOffset = headerHeight + searchHeight;
+
+    $('div.stp-content-body div.container').css({'padding-top': containerOffset});
 }
 
-$(function() {
-  $('a.control_right').on('click', toggleSearch);
+$('a.control_right').click(toggleSearch);
+
+// SIDETAP CONTROLS
+var st = new sidetap();
+    
+$('header a.control_left').click(st.toggle_nav);
+
+st.stp_nav.find('nav a.active').click(function(e) {
+  e.preventDefault();
+  
+  var $self = $(this),
+      destination = $self.attr('href'),
+      $showPanel = $(destination),
+      showPage = function() {
+        adjustContainers();
+        st.toggle_nav();
+        $('div.stp-nav nav a').removeClass('current');
+        st.show_section($showPanel, {
+          animation: 'infromright',
+          callback : function() {
+            $self.addClass('current');
+            if(destination === '#home_unreg') {
+              st.stp_nav.addClass('unreg');
+            } else {
+              st.stp_nav.removeClass('unreg');
+            }
+          }
+        });
+      };
+      
+      //Only Showpage if not current
+      if(!$self.hasClass('current')) {
+        showPage();
+      }
+      
 });
