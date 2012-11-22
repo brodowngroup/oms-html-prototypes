4baa42e6ebb551e245562030d1078c117e62ade8
brodowngroup <mike@codegreendevelopment.com>
2012-11-22 11:56:14 -0600

    splits index into sepatate pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,26 @@
 // HEADER SEARCH TOGGLE
 var toggleSearch = function() {
-  var $form = $('form.header_search'),
-      height = -($form.height());
-        
-  $form.hasClass('active') ? 
-    $form.animate({'top': height}, 100).removeClass('active') :
-    $form.animate({'top': 0}, 100).addClass('active');
+      var $form = $('form.header_search'),
+          $content = $('div.stp-content-frame');
+          height = -($form.innerHeight()),
 
-},
-adjustContainers = function() {
-    var headerHeight = $('header').height(),
-        searchHeight = $('form.header_search').height(),
-        containerOffset = headerHeight + searchHeight;
+          showSearch = function() {
+            $form.animate({'top': 0}, 100).addClass('active');
+            $content.animate({'top': 0}, 100);
+          },
+          
+          hideSearch = function() {
+            $form.animate({'top': height}, 100).removeClass('active');
+            $content.animate({'top': height}, 100);
+          };
+      
+      $form.hasClass('active') ? hideSearch() : showSearch();
 
-    $('div.stp-content-body div.container').css({'padding-top': containerOffset});
-}
+    };
 
-$('a.control_right').click(toggleSearch);
+$('header a.control_right').click(toggleSearch);
 
 // SIDETAP CONTROLS
 var st = new sidetap();
     
 $('header a.control_left').click(st.toggle_nav);
-
-st.stp_nav.find('nav a.active').click(function(e) {
-  e.preventDefault();
-  
-  var $self = $(this),
-      destination = $self.attr('href'),
-      $showPanel = $(destination),
-      showPage = function() {
-        adjustContainers();
-        st.toggle_nav();
-        $('div.stp-nav nav a').removeClass('current');
-        st.show_section($showPanel, {
-          animation: 'infromright',
-          callback : function() {
-            $self.addClass('current');
-            if(destination === '#home_unreg') {
-              st.stp_nav.addClass('unreg');
-            } else {
-              st.stp_nav.removeClass('unreg');
-            }
-          }
-        });
-      };
-      
-      //Only Showpage if not current
-      if(!$self.hasClass('current')) {
-        showPage();
-      }
-      
-});
