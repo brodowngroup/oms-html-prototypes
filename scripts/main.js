7884c6b37808c9a136ee2e0c2ff0e570c2e8d29b
brodowngroup <mike@codegreendevelopment.com>
2013-01-28 21:23:52 -0600

    test getJson

--- scripts/main.js
+++ scripts/main.js
@@ -27,10 +27,47 @@ $('header a.toggleSearch').click(toggleSearch);
 // ---------------
 $('form.header_search').on('submit', function(e) {
   e.preventDefault();
-  var query = $(this).find('input').val(),
-      url = 'api_results.html?term=' + query;
-  window.location = url;
+  var $form = $(this),
+      query = $form.find('input').val(),
+      url = $form.attr('action');
+
+  $.getJSON('scripts/test_json.json', function(data) {
+
+    console.log('success');
+
+  });
+
 });
+
+
+
+
+
+$("#searchForm").submit(function(event) {
+
+  /* stop form from submitting normally */
+  event.preventDefault();
+
+  /* get some values from elements on the page: */
+  var $form = $( this ),
+      term = $form.find( 'input[name="s"]' ).val(),
+      url = $form.attr( 'action' );
+
+  /* Send the data using post */
+  var posting = $.post( url, { s: term } );
+
+  /* Put the results in a div */
+  posting.done(function( data ) {
+    var content = $( data ).find( '#content' );
+    $( "#result" ).empty().append( content );
+  });
+
+
+
+
+
+
+
 
 
 // ---------------
