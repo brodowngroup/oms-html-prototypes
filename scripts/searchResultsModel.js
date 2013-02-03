44151783ed312b6ef7ec4d9f783fb927b2aa932c
brodowngroup <mike@codegreendevelopment.com>
2013-02-03 11:44:21 -0600

    successfully adapting api data to ko search result model

--- scripts/searchResultsModel.js
+++ scripts/searchResultsModel.js
@@ -1,20 +1,31 @@
-// Class to represent a row in the seat reservations grid
-function Result(name) {
-    var self = this;
-    self.name = name;
+// Class that defines an individual
+// event returned from the api
+function Result(data) {  
+  this.id = data.ID;
+  this.name = data.Name;
+  this.image = data.Image;
+  this.date = data.Date;
+  this.time = data.Time;
+  this.venue = data.Venue;
+  this.neighborhood = data.Neighborhood;
+  this.city = data.City;
+  this.state = data.State;
+  this.latitude = data.Latitude;
+  this.longitude = data.Longitude;
 }
 
+// Main Viewmodel for the results page
 function SearchResultsModel() {
-    // Data
-    var self = this;
-    self.results = ko.observableArray([
-        new Result("Tonight"),
-        new Result("Im"),
-        new Result("gonna"),
-        new Result("rock"),
-        new Result("you"),
-        new Result("tonight"),
-    ]);
+  // Data
+  var self = this;
+  self.results = ko.observableArray([]);
+
+  // Get json from api call
+  $.post("http://api.onmystage.net/api/search/", { term: 'mad bread' }, function(data) {
+      var mappedResults = $.map(data, function(item) { return new Result(item) });
+      self.results(mappedResults);
+  }, 'json');
+  
 };
 
 ko.applyBindings(new SearchResultsModel());
