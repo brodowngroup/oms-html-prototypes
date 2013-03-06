d06ba25df50dd7a665739aee4402545986c93895
Slatron <mike@codegreendevelopment.com>
2013-03-05 23:10:33 -0600

    first history js trial

--- scripts/main.js
+++ scripts/main.js
@@ -236,7 +236,7 @@ oms.app.loadPage('home_unreg.html');
 $('div.ko_flicker_fix').add('h2.ko_flicker_fix')
                        .add('button.ko_flicker_fix')
                        .removeClass('ko_flicker_fix');
-
+                       
 // Bind UI Events
 // -------------------
 $('header a.toggleSearch').on('click', oms.toggleSearch);
@@ -264,3 +264,33 @@ $('div.results_area > div').on('click', 'a.event_link', function(e) {
   var index = $(this).prop('rel');
   oms.app.loadEvent(index);
 });
+
+// History Control
+// -----------------
+(function(window,undefined){
+
+    // Prepare
+    var History = window.History; // Note: We are using a capital H instead of a lower h
+    if ( !History.enabled ) {
+         // History.js is disabled for this browser.
+         // This is because we can optionally choose to support HTML4 browsers or not.
+        return false;
+    }
+
+    // Bind to StateChange Event
+    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        History.log(State.data, State.title, State.url);
+    });
+
+    // Change our States
+    History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
+    History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
+    History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
+    History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
+    History.back(); // logs {state:3}, "State 3", "?state=3"
+    History.back(); // logs {state:1}, "State 1", "?state=1"
+    History.back(); // logs {}, "Home Page", "?"
+    History.go(2); // logs {state:3}, "State 3", "?state=3"
+
+})(window);
