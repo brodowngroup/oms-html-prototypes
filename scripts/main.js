c7ba1a323a4ecdcd612f2adae93a271a90540c13
Slatron <mike@codegreendevelopment.com>
2013-03-05 23:12:26 -0600

    commenting out History demo

--- scripts/main.js
+++ scripts/main.js
@@ -284,13 +284,13 @@ $('div.results_area > div').on('click', 'a.event_link', function(e) {
     });
 
     // Change our States
-    History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
-    History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
-    History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
-    History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
-    History.back(); // logs {state:3}, "State 3", "?state=3"
-    History.back(); // logs {state:1}, "State 1", "?state=1"
-    History.back(); // logs {}, "Home Page", "?"
-    History.go(2); // logs {state:3}, "State 3", "?state=3"
+    // History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
+    // History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
+    // History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
+    // History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
+    // History.back(); // logs {state:3}, "State 3", "?state=3"
+    // History.back(); // logs {state:1}, "State 1", "?state=1"
+    // History.back(); // logs {}, "Home Page", "?"
+    // History.go(2); // logs {state:3}, "State 3", "?state=3"
 
 })(window);
